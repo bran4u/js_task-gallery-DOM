c0709f28ff6ef9ce9cfd0c1b33a70b353b90c430
bran4u <s.oselskiy@gmail.com>
2020-06-17 11:01:08 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const gallery = document.querySelector('.gallery');
+const galleryMainImg = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', (e) => {
+  if (e.target.classList.contains('gallery__img')) {
+    e.preventDefault();
+
+    const str = e.target.src.replace('-thumb.jpeg', '.png');
+
+    galleryMainImg.src = str;
+  }
+});
